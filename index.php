cd3c6e49ee445a92e42c558fe4906a362dd785c2
Edenilson João Pereira <ninocriptocoin@gmail.com>
2023-06-08 23:56:03 -0300

    começando...

--- index.php
+++ index.php
@@ -5,8 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Val - Consulta Espiritual</title>
+    <link rel="stylesheet" href="assets/css/val.css">
+    <link rel="shortcut icon" href="">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Val - Consulta Espiritual</h1>
+    <header class="">
+        <h1>Val - Consulta Espiritual</h1>
+    </header>
+    <main class="">
+        <div class="row">
+            <div class="col-10">
+                <p>
+                Val.floripa.br
+                Val 
+                Consultas espirituais.
+                Venha conhecer meu trabalho, na ilha da Magia Florianópolis.
+                seu futuro
+                Conheça e entenda o seu passado para poder tomar decisões sábias no presente e ter um futuro prospero e feliz.
+                Dias
+                Consultas espirituais.
+                Segunda-feira - Quarta feira – Sexta-feira a partir das: 13:30h
+                Quinta-feira a partir das 18:00h
+                Agende seu horário pelo WhatsApp 
+                (48) 99694-4797 ou telefone (48) 3232-4025
+                Terça-feira e sábado – Consulta com Carta de Baralho
+                Somente com horário marcado
+                Saúde, física e mental não se adquire e sim se conquista! 
+                Se você está com problemas de saúde ou buscando compreender melhor seu lado emocional e espiritual, venha me conhecer  e assim como milhares de pessoas mude sua vida para melhor.
+
+                </p>
+            </div>
+        </div>
+    </main>
+    <footer class="">
+
+    </footer>
+    
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </html>
