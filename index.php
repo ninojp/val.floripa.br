c3c0fe24b62d367678992137bf31b224f70ca2c4
Edenilson João Pereira <ninocriptocoin@gmail.com>
2023-06-10 22:35:34 -0300

    varias alterações

--- index.php
+++ index.php
@@ -20,40 +20,66 @@
                 <ol class="ol_menu"><a href="./sobre_val.php" class="link_menu">Sobre</a></ol>
             </ul>
         </div>
-        <div class="col-12 text-center">
-            <h1>Val - Consulta Espiritual</h1>
+        <div class="col-12">
+            <h1 class="h1_titulo">Consulta Espiritual</h1>
         </div>
-        <div class="col-12 text-center">
-            <h2>Venha conhecer meu trabalho, na ilha da Magia Florianópolis</h2>
+        <div class="col-12">
+            <h2 class="h2_subtit">Venha conhecer meu trabalho, na ilha da Magia Florianópolis</h2>
+            <p class="p_texto_desc">
+            A Valdete é uma guia espiritual que oferece consultas para ajudar as pessoas a descobrir mais sobre si mesmas e a encontrar o seu caminho. Com mais de 35 anos de experiência, ela oferece consultas personalizadas para ajudar a desvendar mistérios e desafios da vida. Descubra a sua verdadeira essência com Val e comece a trilhar um caminho de prosperidade e felicidade.
+            </p>
         </div>
     </header>
-    <main class="">
-        <div class="row">
+    <main class="main_page_val">
+        <section class="row section_horario">
             <div class="col-10">
-                <p>
-                Val.floripa.br
-                Val 
-                Consultas espirituais.
-                Venha conhecer meu trabalho, na ilha da Magia Florianópolis.
-                seu futuro
-                Conheça e entenda o seu passado para poder tomar decisões sábias no presente e ter um futuro prospero e feliz.
-                Dias
-                Consultas espirituais.
-                Segunda-feira - Quarta feira – Sexta-feira a partir das: 13:30h
-                Quinta-feira a partir das 18:00h
-                Agende seu horário pelo WhatsApp 
-                (48) 99694-4797 ou telefone (48) 3232-4025
-                Terça-feira e sábado – Consulta com Carta de Baralho
-                Somente com horário marcado
-                Saúde, física e mental não se adquire e sim se conquista! 
-                Se você está com problemas de saúde ou buscando compreender melhor seu lado emocional e espiritual, venha me conhecer  e assim como milhares de pessoas mude sua vida para melhor.
+                <p class="p_horarios">
+                    Localizado na Barra da Lagoa - Florianópolis SC <a href="#sect_local">(Click ->)</a><br>
 
+                Dias do atendimento<br>
+                Segunda-feira - Quarta feira – Sexta-feira a partir das: 13:30h<br>
+                Quinta-feira a partir das 18:00h<br>
+
+                Agende seu horário pelo WhatsApp <br>
+                (48) 99694-4797 ou telefone (48) 3232-4025 <br>
+                
+                Terça-feira e sábado – Consulta com Carta de Baralho<br>
+                Somente com horário marcado
                 </p>
             </div>
-        </div>
+        </section>
+        <section class="section_desc">
+            <div class="col-3 div_descri">
+                <p class="p_descri">
+                    SAÚDE<br>
+                    Saúde, física, mental e espiritual não se adquire e sim se conquista!
+                    Saiba que o equilíbrio entre as três partes que compõem o ser humano deve estar em homeostasia, pois o bem estar físico depende o do seu estado psicológico que está ligado diretamente com seu lado espiritual.
+                </p>
+            </div>
+            <div class="col-3 div_descri">
+                <p class="p_descri">
+                    AMOR<br>                     
+                    Se você está com problemas no seu relacionamentio ou buscando a sua outra metade
+                     compreender melhor seu lado emocional e espiritual, venha me conhecer  e assim como milhares de pessoas mude sua vida para melhor.
+                </p>
+            </div>
+            <div class="col-3 div_descri">
+                <p class="p_descri">
+                    PROFISSIONAL<br>
+                    Conheça e entenda o seu passado para poder tomar decisões sábias no presente e ter um futuro prospero e feliz. 
+                    buscando compreender melhor seu lado emocional e espiritual, venha me conhecer e assim como milhares de pessoas mude sua vida para melhor.
+                </p>
+            </div>
+        </section>
+        <section class="section_local" id="sect_local">
+            <p>Google location</p>
+        </section>
     </main>
-    <footer class="">
-
+    <footer class="row row_footer">
+        <div class="col-3 text-center">2023</div>
+        <div class="col-3 text-center">Tweeter</div>
+        <div class="col-3 text-center">Facebook</div>
+        <div class="col-3 text-center">Instagram</div>
     </footer>
     
 </body>
