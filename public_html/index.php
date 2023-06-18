9859d9495e5e0d4efd5b467311d8b7217900a1e1
Edenilson João Pereira <ninocriptocoin@gmail.com>
2023-06-18 20:33:04 -0300

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="index, follow">
     <meta name="description" content="Val é uma guia espiritual que oferece consultas espirituais para ajudar as pessoas a descobrir mais sobre si mesmas e a encontrar o seu caminho, localizado na ilha da magia Florianópolis" />
-    <link rel="canonical" href="https://val.floripa.br/">
+    <link rel="canonical" href="https://val.floripa.br/index.php">
     <link rel="stylesheet" href="./assets/css/val.css">
     <link rel="shortcut icon" href="./assets/imgs/vidente.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
